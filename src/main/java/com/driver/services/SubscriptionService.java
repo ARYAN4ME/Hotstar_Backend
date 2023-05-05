d66e8c965da93ee091fa07f5f7360ad623b60a9d
Mohit <102155711+ARYAN4ME@users.noreply.github.com>
2023-05-05 18:26:55 +0530

    first commit

--- src/main/java/com/driver/services/SubscriptionService.java
+++ src/main/java/com/driver/services/SubscriptionService.java
@@ -25,8 +25,26 @@ public class SubscriptionService {
     public Integer buySubscription(SubscriptionEntryDto subscriptionEntryDto){
 
         //Save The subscription Object into the Db and return the total Amount that user has to pay
+       User user = userRepository.findById(subscriptionEntryDto.getUserId()).get();
+       Subscription subscription = user.getSubscription();
+       int totalPrice = 0;
+       subscription.setSubscriptionType(subscriptionEntryDto.getSubscriptionType());
+       subscription.setNoOfScreensSubscribed(subscriptionEntryDto.getNoOfScreensRequired());
+//       subscription.setStartSubscriptionDate(new Date());
+       if(subscriptionEntryDto.getSubscriptionType().equals(SubscriptionType.BASIC)){
+           totalPrice = 500 + (subscriptionEntryDto.getNoOfScreensRequired()*200);
+       } else if(subscriptionEntryDto.getSubscriptionType().equals(SubscriptionType.PRO)) {
+           totalPrice = 800 + (subscriptionEntryDto.getNoOfScreensRequired()*250);
+       }
+       else if(subscriptionEntryDto.getSubscriptionType().equals(SubscriptionType.ELITE)){
+           totalPrice = 1000 + (subscriptionEntryDto.getNoOfScreensRequired()*350);
+       }
+       subscription.setTotalAmountPaid(totalPrice);
+       subscription.setUser(user);
+       user.setSubscription(subscription);
+       userRepository.save(user);
 
-        return null;
+        return totalPrice;
     }
 
     public Integer upgradeSubscription(Integer userId)throws Exception{
@@ -34,16 +52,36 @@ public class SubscriptionService {
         //If you are already at an ElITE subscription : then throw Exception ("Already the best Subscription")
         //In all other cases just try to upgrade the subscription and tell the difference of price that user has to pay
         //update the subscription in the repository
+        User user = userRepository.findById(userId).get();
+        int prevPaid = user.getSubscription().getTotalAmountPaid();
+        int currPaid = 0;
+       if(user.getSubscription().getSubscriptionType().equals(SubscriptionType.ELITE)){
+            throw new Exception("Already the best Subscription");
+        }
+       else if(user.getSubscription().getSubscriptionType().equals(SubscriptionType.BASIC)) {
+           currPaid  = 800 + (250 * user.getSubscription().getNoOfScreensSubscribed());
+           user.getSubscription().setSubscriptionType(SubscriptionType.PRO);
+       }
+       else {
+           currPaid  = 1000 + (350 * user.getSubscription().getNoOfScreensSubscribed());
+           user.getSubscription().setSubscriptionType(SubscriptionType.ELITE);
+       }
+       subscriptionRepository.save(user.getSubscription());
+       return currPaid - prevPaid;
 
-        return null;
     }
 
     public Integer calculateTotalRevenueOfHotstar(){
 
         //We need to find out total Revenue of hotstar : from all the subscriptions combined
         //Hint is to use findAll function from the SubscriptionDb
+        int revenue = 0;
+        List<Subscription> revenueList = subscriptionRepository.findAll();
+        for(Subscription find : revenueList){
+            revenue += find.getTotalAmountPaid();
+        }
 
-        return null;
+        return revenue;
     }
 
 }
--- src/main/java/com/driver/services/WebSeriesService.java
+++ src/main/java/com/driver/services/WebSeriesService.java
@@ -2,6 +2,7 @@ package com.driver.services;
 
 import com.driver.EntryDto.WebSeriesEntryDto;
 import com.driver.model.ProductionHouse;
+import com.driver.model.SubscriptionType;
 import com.driver.model.WebSeries;
 import com.driver.repository.ProductionHouseRepository;
 import com.driver.repository.WebSeriesRepository;
@@ -18,13 +19,41 @@ public class WebSeriesService {
     ProductionHouseRepository productionHouseRepository;
 
     public Integer addWebSeries(WebSeriesEntryDto webSeriesEntryDto)throws  Exception{
+        WebSeries series;
+        try{
+             series = webSeriesRepository.findBySeriesName(webSeriesEntryDto.getSeriesName());
+        }catch (Exception e){
+            throw new Exception("Series is already present");
+        }
+        series.setSeriesName(webSeriesEntryDto.getSeriesName());
+        series.setAgeLimit(webSeriesEntryDto.getAgeLimit());
+        series.setRating(webSeriesEntryDto.getRating());
+        series.setSubscriptionType(webSeriesEntryDto.getSubscriptionType());
+
+        ProductionHouse productionHouse;
+        try {
+            productionHouse = productionHouseRepository.findById(webSeriesEntryDto.getProductionHouseId()).get();
+        }catch (Exception e){
+            throw new Exception("productionHouse not found!!!");
+        }
+        series.setProductionHouse(productionHouse);
+        productionHouse.getWebSeriesList().add(series);
+//        webSeriesRepository.save(series);
+
+        double oldRating = productionHouse.getRatings();
+        double newRating = series.getRating();
+        int size = productionHouse.getWebSeriesList().size();
+        double updateRating = oldRating + (newRating - oldRating)/size;
+        productionHouse.setRatings(updateRating);
+        productionHouseRepository.save(productionHouse);
+        webSeriesRepository.save(series);
 
         //Add a webSeries to the database and update the ratings of the productionHouse
         //Incase the seriesName is already present in the Db throw Exception("Series is already present")
         //use function written in Repository Layer for the same
         //Dont forget to save the production and webseries Repo
 
-        return null;
+        return series.getId();
     }
 
 }
